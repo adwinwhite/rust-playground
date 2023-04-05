6b983dc13d0e53686f23ac27b67b401ddc5c5a71
Adwin White <adwinw01@gmail.com>
2023-04-05 20:35:09 +0800

    Only take necessary argument

--- orchestrator/src/bin/worker.rs
+++ orchestrator/src/bin/worker.rs
@@ -4,9 +4,7 @@ use orchestrator::worker::listen;
 
 #[tokio::main(flavor = "current_thread")]
 pub async fn main() {
-    let args: Vec<String> = env::args().collect();
-
     // Panics messages are written to stderr.
-    let project_dir = args.get(1).expect("Please specify project directory as the first argument");
+    let project_dir = env::args().nth(1).expect("Please specify project directory as the first argument");
     listen(project_dir.into()).await.unwrap();
 }
